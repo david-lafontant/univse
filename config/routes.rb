e924d680ec0d9eb54954f6f8156036ea0a704a6e
David Lafontant <lafontant.david@gmail.com>
2023-08-07 16:56:50 -0400

    Add routes for teachers and set root path

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   get 'pages/events'
   get 'pages/dasboard'
   get 'pages/contact'
+  get 'pages/teachers'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'pages#home'
   # Defines the root path route ("/")
   # root "articles#index"
 end
