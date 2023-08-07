6a792e194954ca997042a419f9c32ffdbaf55c15
David Lafontant <lafontant.david@gmail.com>
2023-08-07 16:54:26 -0400

    Add js ressources

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,18 @@ import "./src/jquery"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 
+import "./src/js/main"
+import "./src/js/ajax-contact"
+import "./src/js/jquery.countdown.min"
+import "./src/js/jquery.counterup.min"
+import "./src/js/jquery.magnific-popup.min"
+import "./src/js/jquery.nice-select.min"
+import "./src/js/jquery.nice-number.min"
+import "./src/js/map-script"
+import "./src/js/slick.min"
+import "./src/js/validator.min"
+import "./src/js/jquery.waypoints.min"
+
 $(function(){
   console.log("hello")
 })
