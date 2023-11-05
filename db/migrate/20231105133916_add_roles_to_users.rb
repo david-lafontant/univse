6c8bf4d2cd9ebfc79635aeb6e238878f78de1272
David Lafontant <lafontant.david@gmail.com>
2023-11-05 18:14:14 -0500

    Delete migration

--- db/migrate/20231105133916_add_roles_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddRolesToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :superadmin_role, :boolean
-    add_column :users, :supervisor_role, :boolean
-    add_column :users, :user_role, :boolean
-  end
-end
