a2baf0f9abe6b7fdb9ff8f8e26db15621a382f1f
David Lafontant <lafontant.david@gmail.com>
2023-11-05 01:25:06 -0500

    Set routes for devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  devise_for :users
+
+  devise_scope :user do  
+     get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
   get 'pages/about'
   get 'pages/courses'
   get 'pages/events'
@@ -10,4 +15,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
